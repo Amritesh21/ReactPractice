eb07cb71445d5d8cf62a143b1f21d705fd50dbda
Amritesh21 <amritesh999singh@gmail.com>
2022-02-15 23:22:50 +0530

    Working impementation of router

--- reacttut/src/NavigationComponents/NavigationSection.js
+++ reacttut/src/NavigationComponents/NavigationSection.js
@@ -1,8 +1,25 @@
+
+import { Link } from 'react-router-dom';
 import './NavigationList.css'
 export const NavigationSection = () => {
     const navigationFields = ["Nav1","Nav2","Nav3","Nav4","Nav5"];
+    const navigationLink = ["/","/form","/about","Nav4","Nav5"];
+    const navigationData = [
+        {
+            field : "Home",
+            link : "/"
+        },
+        {
+            field: "Register",
+            link : "/form"
+        },
+        {
+            field: "About",
+            link : "/about"
+        }
+    ]
    // const navVals = NavigationFields.map((NavigationFields) => <li>{NavigationFields}</li>);
-    const listItems =  navigationFields.map((navigationField)  => <li className="navItems"><a href="#">{navigationField}</a></li>)
+    const listItems =  navigationData.map((navigationField)  => <li className="navItems"><Link to={navigationField.link}>{navigationField.field}</Link></li>)
     return(
         <>
             {listItems}
